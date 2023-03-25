06ae5c31af7c7979e6cd5247cff181bc8abdcaed
“viditP1001” <vidit@veridicsolutions.com>
2023-03-25 18:06:22 +0530

    f

---
+++ includes/menu.php
@@ -0,0 +1,52 @@
+<div class="menu-perpective">
+  <button class="btn close" onclick="smartMenu()"> <img src="img/close.svg" alt=""> </button>
+  <div class="p-4 mt-4"></div>
+
+  <div class="row">
+    <div class="col-8 d-none d-sm-block courseOffering">
+      <h4 class="mb-4 opacity-75 AmsiProU text-secondary">COURSES OFFERING</h4>
+      <ul class="AmsiProR">
+        <li>Oracle Fusion Technical</li>
+        <li>Oracle Finance & PPM</li>
+        <li>Oracel HCM Techn-Fun</li>
+        <li>Oracle SCM</li>
+        <li>Oracle Apps Tech</li>
+        <li>Oracle Apex</li>
+        <li>Java</li>
+        <li>SFDC</li>
+        <li>Mobile Android</li>
+        <li>Mobile iOS</li>
+        <li>Adobe AEM/Creative</li>
+        <li>Machine Learning</li>
+        <li>Data Science</li>
+        <li>MS Devops</li>
+        <li>MS Sharepoint</li>
+        <li>MS Power BI</li>
+        <li>Fullstack</li>
+        <li>SAP ABAP</li>
+        <li>SAP FICO</li>
+        <li>SAP MM</li>
+        <li>SAP Hybris</li>
+        <li>SAP Fiori</li>
+      </ul>
+    </div>
+    <div class="col listStyleNone">
+      <ul class="AmsiProU p-0 lh-lg mt-5 fs-5 opacity-75">
+        <li>Home</li>
+        <li>About us</li>      
+        <li>On Premise</li>
+        <li>Online classes</li>
+        <li>Corporate Training</li>
+        <li>College Training</li>
+        <li>Career</li>
+        <li>Terms & Conditions</li>
+        <li>Privacy policy</li>
+      </ul>
+      <div class="">
+        <button type="button" class="btn rounded-0 btn-danger">Login</button>
+        <button type="button" class="btn rounded-0 text-white" style=" background-color: #58C1A3; ">Signup</button>
+      </div>
+    </div>
+  </div>
+
+</div>
--- index.php
+++ index.php
@@ -774,59 +774,8 @@
 </main>
 
 
-<div class="menu-perpective">
-  <button class="btn close" onclick="smartMenu()"> <img src="img/close.svg" alt=""> </button>
-  <div class="p-5 mt-5"></div>
 
-  <div class="row">
-    <div class="col-8 d-none d-sm-block">
-      <h4 class="AmsiProU text-secondary">COURSES OFFERING</h4>
-      <ul class="AmsiProR">
-        <li>Oracle Fusion Technical</li>
-        <li>Oracle Finance & PPM</li>
-        <li>Oracel HCM Techn-Fun</li>
-        <li>Oracle SCM</li>
-        <li>Oracle Apps Tech</li>
-        <li>Oracle Apex</li>
-        <li>Java</li>
-        <li>SFDC</li>
-        <li>Mobile Android</li>
-        <li>Mobile iOS</li>
-        <li>Adobe AEM/Creative</li>
-        <li>Machine Learning</li>
-        <li>Data Science</li>
-        <li>MS Devops</li>
-        <li>MS Sharepoint</li>
-        <li>MS Power BI</li>
-        <li>Fullstack</li>
-        <li>SAP ABAP</li>
-        <li>SAP FICO</li>
-        <li>SAP MM</li>
-        <li>SAP Hybris</li>
-        <li>SAP Fiori</li>
-      </ul>
-    </div>
-    <div class="col listStyleNone">
-      <ul class="AmsiProU p-0 lh-lg">
-        <li>Home</li>
-        <li>About us</li>
-        <br><br><br>
-        <li>On Premise</li>
-        <li>Online classes</li>
-        <li>Corporate Training</li>
-        <li>College Training</li>
-        <li>Career</li>
-        <li>Terms & Conditions</li>
-        <li>Privacy policy</li>
-      </ul>
-      <div class="">
-        <button type="button" class="btn rounded-0 btn-danger">Login</button>
-        <button type="button" class="btn rounded-0 text-white" style=" background-color: #58C1A3; ">Signup</button>
-      </div>
-    </div>
-  </div>
-
-</div>
+<?php include 'includes/menu.php'  ?>
 
 
 
